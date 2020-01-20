35528b97421dc1c8064523ccb346bba2e6f16ddf
GaZi-NK <redman.fox@icloud.com>
2020-01-20 23:22:31 +0900

    完成！！あとは障害物をつけてそれに当たったらゲームオーバーになるようにする！! ⇒隕石を描画し上から落とす処理成功！

--- app/src/main/java/com/example/redma/ballrolling/CanvasView.kt
+++ app/src/main/java/com/example/redma/ballrolling/CanvasView.kt
@@ -4,38 +4,61 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.view.*
 import java.util.jar.Attributes
+import android.graphics.Canvas
+import androidx.annotation.IntegerRes
 
 //カスタムビュー
 class CanvasView(context : Context, attbs : AttributeSet) : View(context, attbs) {
-    private lateinit var paint : Paint
+    private var paint: Paint = Paint()
     private var bmp : Bitmap? = null
     private var xpos : Float = 0f
     private var ypos : Float = 0f
     private var preX : Float = 0f
     private var preY : Float = 0f
+    // 描画するラインの太さ
+    private val lineStrokeWidth = 20f
+    private val strokeW1 = 20f
+    private val strokeW2 = 40f
+
+    //隕石が下に落ちる速度
+    private var rakka = 3.0f
 
     //コンストラクタ
     init {
-        //ポイントのインスタンスを取得
-        paint = Paint()
-
         //ボールの画像を取得
         bmp = BitmapFactory.decodeResource(resources, R.drawable.ball)
     }
 
-    override fun onDraw(canvas: Canvas?) {
-        //背景、半透明
-        canvas?.drawColor(Color.argb(125, 0, 0, 255))
+    override fun onDraw(canvas: Canvas) {
+        //画面縦、横の真ん中の座標を取得
+        val xc = (canvas.width / 2).toFloat()
+        val yc = (canvas.height / 2).toFloat()
 
-        //ボールを画面に表示させる処理
-        canvas?.drawBitmap(bmp!!, canvas?.getWidth()/2+xpos, canvas?.getHeight()/2+ypos,paint)
+        //ボールを画面に表示させる処理⇒getWidth/2は画面横の真ん中の値
+        //canvas.drawBitmap(bmp!!, xc +xpos, yc+ypos,paint)
+
+
+        //円の書式設定
+        paint.color = Color.argb(255, 125, 125, 255)
+        paint.strokeWidth = strokeW1.toFloat()
+        paint.isAntiAlias = true
+        // 円を投入⇒(中心x1座標, 中心y1座標, r半径, point)⇒書式設定みたいなもの
+        canvas.drawCircle(canvas.getWidth()/2+xpos,  canvas.getHeight()/2+ypos, xc / 8, paint)
+
+
+
+        //隕石の投入
+        canvas.drawCircle(xc-xc/2, 0 + rakka, xc / 4, paint)
+        setPositionMeteo()
     }
 
-    //ボールの位置を描画
+    //ボールの位置を再描画
     public fun setPositionBall(xp : Float, yp : Float){
         var dT : Float = 0.8f
 
+        //感度みたいなのもの⇒数字の部分を大きくすれば少し傾けただけで大きく動くようになる
         val ax : Float = -xp*2
         val ay : Float = yp*2
 
@@ -50,7 +73,6 @@ class CanvasView(context : Context, attbs : AttributeSet) : View(context, attbs)
     }
 
     public fun setPositionMeteo() {
-        //TODO(ここで隕石を描画)
+        rakka += (Math.floor(Math.random () * 10) + 3).toFloat()
+        }
     }
-
-}
